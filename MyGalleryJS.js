cec5a32fd86a9c71fad0ff11b303ae5d4d1c445d
PChabowski <117765708+PChabowski@users.noreply.github.com>
2023-07-15 09:46:11 +0200

    upgrade #1
    Add new function in JS
    Add  new class in CSS
    Add new content modal in HTML with help to CSS class and JS

--- MyGalleryJS.js
+++ MyGalleryJS.js
@@ -1,17 +1,42 @@
 window.onload = function() {
 	var gal = document.getElementById("gallery");
 	var images = gal.getElementsByTagName("img");
-	images.onclick = imgChang;
 	for (var i = 0; i < images.length; i++) {
-		images[i].onclick = imgChang;
-	} 
+		images[i].onclick = check;
+	}
+	closeModal();
 };
 
 function imgChang(img) {
-	var bigPicture = document.getElementById("bigpic");
-	var image = img.target;
-	var oldSrc = image.src
+	var picture = document.getElementById("modalImg");
+	var image = img;
+	var src = image.src;
+	var newSrc = src.replace("/min", "/photos");
+	picture.src = newSrc;
+	openModal();
+}
 
-	var newSrc = oldSrc.replace("min", "photos");
-	bigPicture.src = newSrc;
+function openModal() {
+	var modal = document.getElementById("modal-content");
+	modal.classList.replace("modal-close", "modal-open");
+}
+
+function closeModal() {
+	var modal = document.getElementById("modal-content");
+	var close = document.getElementsByClassName("btn-close")[0];
+	close.onclick = function() {
+		modal.classList.replace("modal-open", "modal-close");
+	}
+}
+
+function check(img) {
+	var picture = document.getElementById("modalImg");
+	var image = img.target;
+	var horiz = image.height;
+	if (horiz < 250){
+		picture.classList.replace("img-vertic", "img-horiz");
+	} else {
+		picture.classList.replace("img-horiz", "img-vertic");
+	}
+	imgChang(image);
 }
---
+++ README.md
@@ -0,0 +1,4 @@
+# MyGalleryJS
+
+EN: My simple modal photo gallery, made in JavaScript.
+PL: Moja prosta modalna galeria zdjęć, wykonana w JavaScript.
