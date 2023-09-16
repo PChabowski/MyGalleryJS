01a0820bfd03d2d4276aebcf0b99f6592fd354bb
PChabowski <117765708+PChabowski@users.noreply.github.com>
2023-09-16 11:36:48 +0200

    upgrade #2
    1.HTML:
    # Remove all tags <img>.
    2.CSS:
    # In modal-content class remove a comment
    3.JavaScript:
    # In window.onload remove loop who add onclick to image tags,
    # In window.onload add function addImage,
    # to table with image name,
    # Make to function addImage,
    # Make function onclickToImg,

--- MyGalleryJS.js
+++ MyGalleryJS.js
@@ -1,42 +1,53 @@
-window.onload = function() {
-	var gal = document.getElementById("gallery");
-	var images = gal.getElementsByTagName("img");
-	for (var i = 0; i < images.length; i++) {
-		images[i].onclick = check;
-	}
+window.onload = () => {
+	addImage(nameImage);
 	closeModal();
 };
 
-function imgChang(img) {
-	var picture = document.getElementById("modalImg");
-	var image = img;
-	var src = image.src;
-	var newSrc = src.replace("/min", "/photos");
+const nameImage = ["426A7148.jpg", "426A7147.jpg", "426A7151.jpg", "426A7152.jpg", "426A7158.jpg", "426A7162.jpg", "426A7163.jpg", "426A7165.jpg", "426A7187.jpg", "IMG_7047.jpg", "IMG_7038.jpg", "IMG_7033.jpg", "426A7168.jpg", "IMG_7050.jpg"];
+
+const addImage = (nameImage) => {
+	const gal = document.getElementById("gallery");
+	for (let i=0; i<nameImage.length; i++) {
+		gal.innerHTML += `<img id="img${i}" src="min/${nameImage[i]}">`;
+	}
+	onclickToImg();
+}
+
+const onclickToImg = () => {
+	const gal = document.getElementById("gallery");
+	const images = gal.getElementsByTagName("img");
+	for (let i = 0; i < images.length; i++) {
+		images[i].onclick = check;
+	}
+}
+
+const imgChang = (img) => {
+	const picture = document.getElementById("modalImg");
+	const src = img.src;
+	const newSrc = src.replace("/min", "/photos");
 	picture.src = newSrc;
 	openModal();
 }
 
-function openModal() {
-	var modal = document.getElementById("modal-content");
+const openModal = () => {
+	const modal = document.getElementById("modal-content");
 	modal.classList.replace("modal-close", "modal-open");
 }
 
-function closeModal() {
-	var modal = document.getElementById("modal-content");
-	var close = document.getElementsByClassName("btn-close")[0];
+const closeModal = () => {
+	const modal = document.getElementById("modal-content");
+	const close = document.getElementsByClassName("btn-close")[0];
 	close.onclick = function() {
 		modal.classList.replace("modal-open", "modal-close");
 	}
 }
 
-function check(img) {
-	var picture = document.getElementById("modalImg");
-	var image = img.target;
-	var horiz = image.height;
-	if (horiz < 250){
+const check = (img)  => {
+	const picture = document.getElementById("modalImg");
+	if (img.target.height < 250){
 		picture.classList.replace("img-vertic", "img-horiz");
 	} else {
 		picture.classList.replace("img-horiz", "img-vertic");
 	}
-	imgChang(image);
+	imgChang(img.target);
 }
